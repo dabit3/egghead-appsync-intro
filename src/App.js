f9adb0389ffa0cc4ded9f7c92eb31cffc5b5cf5c
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-05-23 12:37:57 -0700

    updated to show mutations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
+const createTodo = gql`
+  mutation createTodo($name: String!, $completed: Boolean!) {
+    createTodo(input: {
+      name: $name
+      completed: $completed
+    }) {
+      id
+    }
+  }
+`
 
 class App extends Component {
+  state = { todo: '' }
+  addTodo = () => {
+    if (this.state.todo === '') return
+    const todo = {
+      name: this.state.todo,
+      completed: false
+    }
+    this.props.addTodo(todo)
+    this.setState({ todo: '' })
+  }
   render() {
     return (
       <div className="App">
@@ -10,12 +33,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <input
+          onChange={e => this.setState({ todo: e.target.value })}
+          value={this.state.todo}
+        />
+        <button onClick={this.addTodo}>
+          Add Todo
+        </button>
+      </div> 
     );
   }
 }
 
-export default App;
+export default graphql(createTodo, {
+  props: props => ({
+    addTodo: todo => {
+      props.mutate({ variables: todo })
+    }
+  })
+})(App)
