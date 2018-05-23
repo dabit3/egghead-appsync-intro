07218b29e4b861072abfbd58afb48d7752c04ed7
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-05-23 12:40:13 -0700

    updated to show queries

--- src/App.js
+++ src/App.js
@@ -4,51 +4,34 @@ import './App.css';
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-const createTodo = gql`
-  mutation createTodo($name: String!, $completed: Boolean!) {
-    createTodo(input: {
-      name: $name
-      completed: $completed
-    }) {
-      id
+const query = gql`
+  query listTodos {
+    listTodos {
+      items {
+        id
+        name
+        completed
+      }
     }
   }
 `
 
 class App extends Component {
-  state = { todo: '' }
-  addTodo = () => {
-    if (this.state.todo === '') return
-    const todo = {
-      name: this.state.todo,
-      completed: false
-    }
-    this.props.addTodo(todo)
-    this.setState({ todo: '' })
-  }
   render() {
+    console.log('props: ', this.props)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <input
-          onChange={e => this.setState({ todo: e.target.value })}
-          value={this.state.todo}
-        />
-        <button onClick={this.addTodo}>
-          Add Todo
-        </button>
       </div> 
     );
   }
 }
 
-export default graphql(createTodo, {
-  props: props => ({
-    addTodo: todo => {
-      props.mutate({ variables: todo })
-    }
-  })
+export default graphql(query, {
+  options: {
+    fetchPolicy: 'cache-and-network'
+  }
 })(App)
