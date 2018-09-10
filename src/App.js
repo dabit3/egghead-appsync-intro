f9ea259be5a84b763c5471d059f13e399a04d520
nader dabit <dabit3@gmail.com>
2018-09-10 18:15:27 -0500

    updated mutations

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import gql from 'graphql-tag'
 import { graphql, compose } from 'react-apollo'
 import { graphqlMutation } from 'aws-appsync-react'
 
-const createTodo = gql`
-  mutation createTodo($title: String!, $completed: Boolean!) {
+const ListTodos = gql`
+  query listTodos {
+    listTodos {
+      items {
+        id
+        title
+        completed
+      }
+    }
+  }
+`
+
+const CreateTodo = gql`
+  mutation($title: String!, $completed: Boolean) {
     createTodo(input: {
-      title: $name
+      title: $title
       completed: $completed
     }) {
       id title completed
@@ -24,7 +36,7 @@ class App extends Component {
       title: this.state.todo,
       completed: false
     }
-    this.props.addTodo(todo)
+    this.props.createTodo(todo)
     this.setState({ todo: '' })
   }
   render() {
@@ -41,18 +53,24 @@ class App extends Component {
         <button onClick={this.addTodo}>
           Add Todo
         </button>
+        {
+          this.props.todos.map((todo, index) => (
+            <p key={index}>{todo.title}</p>
+          ))
+        }
       </div> 
     );
   }
 }
 
 export default compose(
-  graphqlMutation(CreatePost, query, 'Todo'),
-  graphql(createTodo, {
+  graphqlMutation(CreateTodo, ListTodos, 'Todo'),
+  graphql(ListTodos, {
+    options: {
+      fetchPolicy: 'cache-and-network'
+    },
     props: props => ({
-     graphqlMutation(CreatePost, listPosts, 'Post') addTodo: todo => {
-        props.mutate({ variables: todo })
-      }
+      todos: props.data.listTodos ? props.data.listTodos.items : []
     })
   })
 )(App)
