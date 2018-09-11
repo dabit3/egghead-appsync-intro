c0b8d6c36e625d7c2fc11f8232fd6c472839f4a1
nader dabit <dabit3@gmail.com>
2018-09-11 17:38:46 -0500

    updated gitignore

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+export default {
+  graphqlEndpoint: "https://5i2avssb5jhhvp7amfs4i2o5au.appsync-api.us-west-2.amazonaws.com/graphql",
+  region: "us-west-2",
+  authenticationType: "API_KEY",
+  apiKey: "da2-hslorzvcqnfh3mjk5uwj7p4tbm"
+}
